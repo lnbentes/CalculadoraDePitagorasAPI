61cc24c7c1e3e649cdabbacd6e3f73090cc5c438
lucas bentes <lucasbentes16@gmail.com>
2022-07-16 22:20:06 -0300

    convertendo para json

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import string
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route("/numero/<numero1>", methods=["GET"])
-def calcular(numero1):
-    numero = numero1
-    return numero
-
-
-@app.route("/numero")
-def bola():
-    return "usuario"
+@app.route("/calcular/<numero1>/<numero2>", methods=["GET"])
+def calcular(numero1, numero2):
+    numero = {"numero1": numero1,
+              "numero2": numero2}
+    return jsonify(numero)
 
 
 app.run()
